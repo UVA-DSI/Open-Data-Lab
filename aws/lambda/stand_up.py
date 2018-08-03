5962bfaf07f261f202d03343135204332992b16d
Max Levinson <mlev71@users.noreply.github.com>
2018-08-03 14:30:22 -0400

    Updating Standup to running version

--- aws/lambda/stand_up.py
+++ aws/lambda/stand_up.py
@@ -1,83 +1,86 @@
+import os
 import boto3
-import botocore
-import logging
-import datetime
 import json
+import datetime
 
-logs = loggin.getLogger('standUp')
-logs.setLevel(loggin.INFO)
-
-ec2_resource = boto3.resource('ec2', region_name='us-east-1')
-subnet = ec2_resource.Subnet('subnet-6fbabc40')
-
-ec2_client = boto3.client('ec2')
 s3_client = boto3.client('s3')
+ec2_resource = boto3.resource('ec2')
+subnet = ec2_resource.Subnet('subnet-1025015b')
 
-def standUp(event, context):
-    ''' Start up an EC2 Instance Fusemounted to Open Data Lab Bucket 
-    '''
-
-    projectId = event.get('projectId')
-    if projectId is None:
-        return "Please Specify a projectId"
-
+def lambda_handler(event, context):
+    # access the sys environment secret keys 
+    access_key = os.environ.get('ACCESS_KEY')
+    secret_key = os.environ.get('SECRET_KEY')
+    
+    # read details from event passed
+    projectId = str(event.get('projectId'))
+    projectKey = str(event.get('projectId'))+'.json'
+    
+    
+    if projectKey is None:
+        return "Please Provide a Project Id"
+    
+    # load project details from s3
     try:
-        projectData = json.loads(
-                s3_client.get_object(Bucket='odltest-projects', Key=str(projectId)+'.json').get('Body').read()
-                )
-
-        # Grab User Credentials from Event
-        access_key = projectData.get('accessKey')
-        secret_key = projectData.get('secretKey')
-        bucket = projectData.get('data')
-        github = projectData.get('github')
-
-    except:
-        return "Unable to Find Project Data"
-
-
-    # Generate SHH Credentials
-    key_pair = KeyPair('AutoKey:'+str(datetime.datetime.now()))
-
-
-    # Template the Bash Script
+        projectS3 = s3_client.get_object(Bucket='odltest-projects', Key=projectKey).get('Body').read()
+        assert projectS3 is not None
+        projectDetails = json.loads(projectS3.decode('utf-8'))
+        assert projectDetails is not None
+    except AssertionError:
+        return "Project Datails not found for project:{}".format(projectKey)
+    
+    # generate a new key pair for EC2 instance
+    key_name = 'key_'+str(int(datetime.datetime.timestamp(datetime.datetime.now())))
+    
+    key_pair = ec2_resource.create_key_pair(
+        KeyName = key_name
+    )
+    
+    
+    # template UserData bash script to auto fusemount
     init_script = """#!/bin/bash
-    sudo su
-    yum install -y gcc libstdc++-devel gcc-c++ fuse fuse-devel curl-devel libxml2-devel mailcap automake openssl-devel git
-    git clone https://github.com/s3fs-fuse/s3fs-fuse
-    cd s3fs-fuse/
-    ./autogen.sh
-    ./configure --prefix=/usr --with-openssl
-    make
-    make install
-    touch /etc/passwd-s3fs
-    echo {}:{} | tee /etc/passwd-s3fs
-    chmod 400 /etc/passwd-s3fs
-    mkdir ~/data
-    s3fs odltest-mal ~/data -o use_cache=/tmp
-    cd ~/
-    git clone {}
-    """.format(access_key, secret_key, bucket_name, github)
-
-
-    # create a new instance
+sudo su
+yum install -y gcc libstdc++-devel gcc-c++ fuse fuse-devel curl-devel libxml2-devel mailcap automake openssl-devel git
+git clone https://github.com/s3fs-fuse/s3fs-fuse
+cd s3fs-fuse/
+./autogen.sh
+./configure --prefix=/usr --with-openssl
+make
+make install
+touch /etc/passwd-s3fs
+echo {}:{} | tee /etc/passwd-s3fs
+chmod 400 /etc/passwd-s3fs
+mkdir ~/data
+s3fs odltest-mal ~/data -o use_cache=/tmp
+cd ~/
+git clone {}
+    """.format(access_key, secret_key, projectDetails.get('data'), projectDetails.get('github'))
+    
     instance = subnet.create_instances(
-            ImageId = 'ami-b70554c8',
-            InstanceType = 't2.small',
-            MaxCount = 1,
-            MinCount = 1,
-            KeyName = key_pair.name,
-            UserData= init_script
-            )
-
+    ImageId = 'ami-b70554c8',
+    InstanceType = 't2.small',
+    MaxCount = 1,
+    MinCount = 1,
+    KeyName = key_pair.name,
+    UserData = init_script
+    )
+    
     try:
         assert type(instance) == list
         assert len(instance) != 0
-    except AssertionErroro:
+    except AssertionError:
         return "Couldn't Launch Instance"
-
-    publicIp = instance[0].network_interfaces_attribute[0].get('Association', {}).get('PublicIp') 
-
-    return "cat '{0}' > {1} && chmod 400 {1}  && ssh -i {1} ubuntu@{}".format(key_pair.key_material, key_pair.name+'.pem', publicIp)
-
-
+    
+    # add tags to describe the 
+    instance[0].create_tags(
+        Tags = [
+            {
+                'Key': 'ProjectId',
+                'Value': projectId,
+            }
+            ]
+        )
+    
+    publicIp = instance[0].network_interfaces_attribute[0].get('Association', {}).get('PublicDnsName')
+    
+    return "echo '{0}' > {1} && chmod 400 {1}  && ssh -i {1} ec2-user@{2}".format(key_pair.key_material, key_pair.name+'.pem', publicIp)
