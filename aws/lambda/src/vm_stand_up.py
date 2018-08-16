cb8ffcc035e065b3e9ae30eae0e54f7e5d8fc768
pete <pete.alonzi@gmail.com>
2018-08-16 14:27:55 -0400

    Update vm_stand_up.py

--- aws/lambda/src/vm_stand_up.py
+++ aws/lambda/src/vm_stand_up.py
@@ -2,6 +2,9 @@ import boto3
 from botocore.exceptions import ClientError
 import datetime
 import time
+import json
+import os
+
 
 
 ec2_resource = boto3.resource('ec2')
@@ -19,11 +22,56 @@ def lambda_handler(event, context):
         KeyName = key_name
     )
     
+    # read details from event passed
+    projectId = '1' #str(event.get('1'))
+    projectKey = '1.json' #str(event.get('1'))+'.json'
+    if projectId is None or projectId=="None":
+        return "Please Provide a Project Id"
+
+    print(projectKey)
+
+    s3_client = boto3.client('s3')
+#    s3_access_key = os.environ.get('AWS_ACCESS_KEY')
+#    s3_secret_key = os.environ.get('AWS_SECRET_KEY')
+#    s3_access_key = os.getenv('AWS_ACCESS_KEY')
+#    s3_secret_key = os.getenv('AWS_SECRET_KEY')
+#    s3_access_key = os.environ('AWS_ACCESS_KEY')
+#    s3_secret_key = os.environ('AWS_SECRET_KEY')
+#    print('access key: ',s3_access_key)
+#    print('secret key: ',s3_secret_key)
+    
+    try:
+        projectS3 = s3_client.get_object(Bucket='odl-projects-test', Key=projectKey).get('Body').read()
+        projectDetails = json.loads(projectS3.decode('utf-8'))
+        assert projectS3 is not None
+        assert projectDetails is not None
+    except AssertionError:
+        return "Project Datails not found for project:{}".format(projectKey)
+
+ 
     # template UserData bash script to auto fusemount
     init_script = """#!/bin/bash
+                     sudo yum install -y gcc libstdc++-devel gcc-c++ fuse fuse-devel curl-devel libxml2-devel mailcap automake openssl-devel git
                      chmod u+x ~/.bash_profile
-                     echo 'mkdir ~/scratch' >> /home/ec2-user/.bash_profile
-                     """
+                     echo ''
+
+                     mkdir /home/ec2-user/data
+                     mkdir /home/ec2-user/scratch
+                     sudo git clone https://github.com/s3fs-fuse/s3fs-fuse
+                     cd s3fs-fuse/
+                     sudo ./autogen.sh
+                     sudo ./configure --prefix=/usr --with-openssl
+                     sudo make
+                     sudo make install
+                     touch /etc/passwd-s3fs
+                     echo {}:{} | tee /etc/passwd-s3fs
+                     chmod 400 /etc/passwd-s3fs
+                     s3fs {} /home/ec2-user/data -o use_cache=/tmp &> /home/ec2-user/mount1.log
+                     s3fs {} /home/ed2-user/scratch -o use_cache=/tmp &> /home/ec2-user/mount2.log
+                     
+                     echo 'git clone {}' >> /home/ec2-user/.bash_profile
+                     """.format(os.environ.get('ACCESS'), os.environ.get('SECRET'), projectDetails.get('data'), projectDetails.get('scratch'), 'https://github.com/UVA-DSI/Open-Data-Lab.git')
+    
                      
                      
                      
@@ -124,7 +172,7 @@ def lambda_handler(event, context):
             },
             Source=SENDER,
         )
-        # Display an error if something goes wrong.
+    # Display an error if something goes wrong.	
     except ClientError as e:
         print(e.response['Error']['Message'])
     else:
@@ -144,6 +192,7 @@ ec2_resource = boto3.resource('ec2')
 subnet = ec2_resource.Subnet('subnet-6fbabc40')
 
 # access the sys environment secret keys 
+import os
 access_key = os.environ.get('ACCESS')
 secret_key = os.environ.get('SECRET')
 
@@ -161,6 +210,7 @@ def standUp(event, context):
     
     
     # load project details from s3
+    s3_client = boto3.client('s3')
     try:
         projectS3 = s3_client.get_object(Bucket='odl-projects-test', Key=projectKey).get('Body').read()
         projectDetails = json.loads(projectS3.decode('utf-8'))
@@ -186,9 +236,9 @@ sudo ./autogen.sh
 sudo ./configure --prefix=/usr --with-openssl
 sudo make
 sudo make install
-sudo touch /etc/passwd-s3fs
-sudo echo {}:{} | sudo tee /etc/passwd-s3fs
-echo 'sudo chmod 400 /etc/passwd-s3fs' >> /home/ec2-user/.bash_profile
+echo 'touch .passwd-s3fs' >> /home/ec2-user/.bash_profile
+echo 'echo {}:{} | tee /home/ec2-user/.passwd-s3fs' >> /home/ec2-user/.bash_profile
+echo 'chmod 400 /home/ec2-user/.passwd-s3fs' >> /home/ec2-user/.bash_profile
 echo 'mkdir ~/data/' >> /home/ec2-user/.bash_profile
 echo 's3fs {} ~/data -o use_cache=/tmp' >> /home/ec2-user/.bash_profile
 echo 'mkdir ~/scratch' >> /home/ec2-user/.bash_profile
@@ -316,7 +366,7 @@ try:
         # following line
         ConfigurationSetName=CONFIGURATION_SET,
     )
-    # Display an error if something goes wrong.
+# Display an error if something goes wrong.	
 except ClientError as e:
     print(e.response['Error']['Message'])
 else:
