34b9fcb8989ef96181ec3ba6211c414e9c497458
pete alonzi <lpa2a@virginia.edu>
2018-10-26 10:24:44 -0400

    update transfer script

--- aws/large_s3_transfer/transferscript.py
+++ aws/large_s3_transfer/transferscript.py
@@ -8,8 +8,16 @@ for line in open('acutal_files_to_transfer.txt'):
 
 tot = len(files)
 
+files2 = []
 for file in files:
+    files2.append(file[:-1])
+
+files2 = files2[0:3]
+print(files2)
+
+for file in files2:
   print('copying {}'.format(file))
-  print('file {} of {}'.format(files.index(file),tot))
+  print('file {} of {}'.format(files2.index(file),tot))
+  os.system("rm -rf /tmp/*")
   os.system("aws s3 cp {} ~/data/. --profile uva_s3".format(file))
   os.system("rm -rf /tmp/*")
