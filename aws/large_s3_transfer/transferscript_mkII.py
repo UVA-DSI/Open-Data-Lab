6aa8f48a5d7939fc5ffa7967ebd8828ed4926309
pete alonzi <lpa2a@virginia.edu>
2018-11-01 11:51:18 -0400

    update transer mk ii

--- aws/large_s3_transfer/transferscript_mkII.py
+++ aws/large_s3_transfer/transferscript_mkII.py
@@ -13,21 +13,15 @@ for file in files:
     files2.append(file[:-1])
 
 
-files2 = files2[2:]
+files2 = files2[3840:3842]
 #print(files2)
 
 
 if True:
     for file in files2:
-	print('copying {} of {}'.format(files2.index(file),tot))
+        print('copying {} of {}'.format(files2.index(file),tot))
         filename = file[file.rfind('hmri-results/')+13:]
         print('copying {}'.format(filename))
-        cmd1 = '{}'.format("aws s3 cp s3://hmri-results/{} . --profile uva_s3".format(filename))
-        cmd2 = '{}'.format("aws s3 cp {} s3://odl-hmtt/{} --profile odl".format(filename[filename.rfind('/')+1:],filename))
-        cmd3 = '{}'.format('rm {}'.format(filename[filename.rfind('/')+1:]))
+        cmd1 = '{}'.format("aws s3 cp s3://hmri-results/{} s3://odl-hmtt/{} --profile uva_s3".format(filename,filename))
         print(cmd1)
         os.system(cmd1)
-        print(cmd2)
-        os.system(cmd2)
-        print(cmd3)
-        os.system(cmd3)
